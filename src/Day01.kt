95b51da346121de1efe0b9b62f109b9b891d44aa
Edu <revilofe@gmail.com>
2023-12-07 17:21:48 +0100

    Solución de primera parte. La segunda: Considerando primer número y último número solamente. Otra forma, pero tampoco funciona. El problema está en cuando se solapan. Por ejemplo oneigth deberia devolver 18. Tal y como lo he hecho, devuelve 11 o 88 dependiendo de por donde empiece a analizar.
    53853

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,80 +1,55 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.sumOf { linea ->
-            linea.filter { caracter ->
-                caracter.isDigit()
-            }.let { num ->
-                if (num.length > 1) "" + num[0] + num[num.length - 1] else "" + num[0] + num[0]
-            }.toInt()
-        }
+    fun calibrationNumber(calibrationText: String): Int {
+        val firsNumber = calibrationText.first { it.isDigit() }
+        val lastNumber = calibrationText.last { it.isDigit() }
+        return "${firsNumber}${lastNumber}".toInt().also { numero ->  println("$calibrationText -> $numero")  }
+    }
+
+    fun solvePart1(input: List<String>): Int {
+        return input.sumOf { calibrationNumber(it) }
     }
 
 
-    fun part2(input: List<String>): Int {
-        val transformaciones = listOf("one" to '1' , "two" to '2', "three" to '3', "four" to '4', "five" to '5', "six" to '6', "seven" to '7', "eight" to '8', "nine" to '9')
-
-        return input.map { linea ->
-            var nuevaLinea = linea
-            print("linea: $linea")
-
-            val transformacionesEncontradas: MutableList<Pair<Int,Pair<String, Char>>> = ArrayList()
-
-            transformaciones.forEach { transformacion ->
-                var indice = linea.indexOf(transformacion.first, 0, true)
-                while (indice != -1)
-                {
-                    transformacionesEncontradas.add(indice to transformacion)
-                    indice = linea.indexOf(transformacion.first, indice + transformacion.first.length, true)
-                }
-            }
-            transformacionesEncontradas.filter { transformacionFiltro ->
-                transformacionFiltro.first != -1
-            }.sortedBy { transformacionOrden ->
-                transformacionOrden.first
-            }.filterIndexed { index, _ ->
-                index == 0 || index == transformacionesEncontradas.size - 1
-            }.let { transformacionesEncontradasOrdenadas ->
-                var nuevaLinea = linea
-                if (transformacionesEncontradasOrdenadas.size > 0) {
-                    val transformacionPrimera = transformacionesEncontradasOrdenadas[0]
-                    nuevaLinea = nuevaLinea.replaceRange(
-                        nuevaLinea.indexOf(transformacionPrimera.second.first),
-                        nuevaLinea.indexOf(transformacionPrimera.second.first) + transformacionPrimera.second.first.length,
-                        transformacionPrimera.second.second.toString()
-                    )
-                }
-                if (transformacionesEncontradasOrdenadas.size > 1) {
-                    val transformacionUltima = transformacionesEncontradasOrdenadas[1]
-                    nuevaLinea = nuevaLinea.reversed()
-                    if (nuevaLinea.indexOf(transformacionUltima.second.first.reversed()) != -1)
-                        nuevaLinea = nuevaLinea.replaceRange(
-                            nuevaLinea.indexOf(transformacionUltima.second.first.reversed()),
-                            nuevaLinea.indexOf(transformacionUltima.second.first.reversed()) + transformacionUltima.second.first.length,
-                            transformacionUltima.second.second.toString()
-                        ).reversed()
-                    else
-                        nuevaLinea = nuevaLinea.reversed()
-                }
-                println("->:  $nuevaLinea")
-                nuevaLinea
-            }
-        }.sumOf { lineaNumeros ->
-            print("linea numeros: $lineaNumeros")
-            lineaNumeros.filter { caracter ->
-                caracter.isDigit()
-            }.let { num ->
-                val n = if (num.length > 1) "" + num[0] + num[num.length - 1] else "" + num[0] + num[0]
-                println("->    n: $n")
-                n
-            }.toInt()
+    fun transformation(calibrationText: String, t:List<String>): String {
+        var calText = calibrationText
+        val tRev = t.map { it.reversed() }
+        val begin = calText.findAnyOf(t) ?: (Int.MAX_VALUE to "")
+        if (begin.first < calText.length)
+            calText = calText.replaceFirst(begin.second, (t.indexOf(begin.second) + 1).toString())
+            val end = calText.reversed().findAnyOf(tRev) ?: (Int.MAX_VALUE to "")
+            if (end.first < calText.length) {
+                calText = calText.reversed().replaceFirst(end.second, (tRev.indexOf(end.second) + 1).toString()).reversed()
         }
+        return calText
     }
 
-    // test if implementation meets criteria from the description, like:
-    // val testInput = readInput("Day01_test")
-    // check(part1(testInput) == 1)
+    fun solvePart2(input: List<String>): Int {
+        val transformaciones = listOf(
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+        )
+        return input.sumOf {
+            calibrationNumber(
+                transformation(it, transformaciones)
+            )
+        }
+
+    }
+
+// test if implementation meets criteria from the description, like:
+// val testInput = readInput("Day01_test")
+// check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    //part1(input).println()
-    part2(input).println()
+    val result = solvePart1(input)
+    check(result == 54953)
+    result.println()
+    solvePart2(input).println() //53868. Correct! //53748
 }
