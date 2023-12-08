22ff95eef0f65c89f6b9f40b24ea3d16eb588fa8
Edu <revilofe@gmail.com>
2023-12-08 01:29:58 +0100

    Day 1: Solución de primera parte. Solución de segunda parte.
    53868

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,55 +1,62 @@
 fun main() {
-    fun calibrationNumber(calibrationText: String): Int {
-        val firsNumber = calibrationText.first { it.isDigit() }
-        val lastNumber = calibrationText.last { it.isDigit() }
-        return "${firsNumber}${lastNumber}".toInt().also { numero ->  println("$calibrationText -> $numero")  }
+
+    val digitsNumbers = mapOf(
+        "1" to "1",
+        "2" to "2",
+        "3" to "3",
+        "4" to "4",
+        "5" to "5",
+        "6" to "6",
+        "7" to "7",
+        "8" to "8",
+        "9" to "9"
+    )
+    val textNumbers = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9"
+    )
+
+    fun firstNumber(input: String, t: List<String>): String {
+        val found = input.findAnyOf(t) ?: (Int.MAX_VALUE to "")
+        return found.second
     }
+
+    fun calibrationNumber(input: String, numbers: Map<String, String>): Int {
+        var number: String = ""
+        val first = numbers[firstNumber(input, numbers.keys.toList())]
+        val last = numbers[firstNumber(input.reversed(), numbers.keys.map { it.reversed() }).reversed()]
+        number += first + last
+        return ("" + number.first() + number.last()).toInt().also { res -> println("$input -> $res") }
+    }
+
 
     fun solvePart1(input: List<String>): Int {
-        return input.sumOf { calibrationNumber(it) }
-    }
-
-
-    fun transformation(calibrationText: String, t:List<String>): String {
-        var calText = calibrationText
-        val tRev = t.map { it.reversed() }
-        val begin = calText.findAnyOf(t) ?: (Int.MAX_VALUE to "")
-        if (begin.first < calText.length)
-            calText = calText.replaceFirst(begin.second, (t.indexOf(begin.second) + 1).toString())
-            val end = calText.reversed().findAnyOf(tRev) ?: (Int.MAX_VALUE to "")
-            if (end.first < calText.length) {
-                calText = calText.reversed().replaceFirst(end.second, (tRev.indexOf(end.second) + 1).toString()).reversed()
-        }
-        return calText
+        return input.sumOf { calibrationNumber(it, digitsNumbers) }
     }
 
     fun solvePart2(input: List<String>): Int {
-        val transformaciones = listOf(
-            "one",
-            "two",
-            "three",
-            "four",
-            "five",
-            "six",
-            "seven",
-            "eight",
-            "nine"
-        )
-        return input.sumOf {
-            calibrationNumber(
-                transformation(it, transformaciones)
-            )
-        }
-
+        return input.sumOf { calibrationNumber(it, digitsNumbers + textNumbers) }
     }
 
-// test if implementation meets criteria from the description, like:
-// val testInput = readInput("Day01_test")
-// check(part1(testInput) == 1)
 
+    // Lee los datos
     val input = readInput("Day01")
-    val result = solvePart1(input)
+
+    // Primera parte
+    var result = solvePart1(input)
     check(result == 54953)
     result.println()
-    solvePart2(input).println() //53868. Correct! //53748
+
+
+    // Segunda parte
+    result = solvePart2(input)
+    check(result == 53868)
+    result.println()
 }
